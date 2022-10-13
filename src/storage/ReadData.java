9581ce43cee3bb1a5c35b638c7f5b22b5b19edf7
manhhoang98 <110440251+manhhoang98@users.noreply.github.com>
2022-10-13 11:20:11 +0700

    Thi Module 2

--- src/storage/ReadData.java
+++ src/storage/ReadData.java
@@ -13,13 +13,14 @@ import static controller.StudentManage.students;
 public class ReadData {
     public static ArrayList<Student> readFileStudentList() {
         try {
-            File file = new File("FileProduct.csv");
+            File file = new File("FileStudent.csv");
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             String value;
             while ((value = bufferedReader.readLine()) != null) {
                 String[] strings = value.split(",");
                 Student student = new Student(Integer.parseInt(strings[0]),strings[1],Integer.parseInt(strings[3]),strings[4],strings[5],Double.parseDouble(strings[5]));
                 students.add(student);
+                System.out.println("Đọc thành công");
             }
             bufferedReader.close();
         } catch (IOException e) {
--- src/storage/WriteData.java
+++ src/storage/WriteData.java
@@ -12,14 +12,15 @@ import static controller.StudentManage.students;
 public class WriteData {
     public static void writeFileStudent(String File) {
         try {
-            java.io.File file = new File("FileProduct.csv");
+            java.io.File file = new File("FileStudent.csv");
             if (!file.exists()) {
                 file.createNewFile();
             }
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
             for (Student student: students) {
-                bufferedWriter.write(student.getStudentCode()+","+student.getName()+","+student.getAge()+","+student.getGender()+","+student.getAddress()+","+student.getMediumScore());
+                bufferedWriter.write(student.getStudentCode()+","+student.getName()+","+student.getAge()+","+student.getGender()+","+student.getAddress()+","+student.getMediumScore()+"\n");
             }
+            System.out.println("Viết thành công");
             bufferedWriter.close();
         }catch (IOException e){
             System.out.println("");
