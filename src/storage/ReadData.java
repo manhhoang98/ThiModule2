bb6142495878ccf63d8f270e3dc1316850e1cc51
manhhoang98 <110440251+manhhoang98@users.noreply.github.com>
2022-10-13 11:31:06 +0700

    Thi Module 2

--- src/storage/ReadData.java
+++ src/storage/ReadData.java
@@ -13,12 +13,12 @@ import static controller.StudentManage.students;
 public class ReadData {
     public static ArrayList<Student> readFileStudentList() {
         try {
-            File file = new File("FileStudent.csv");
+            File file = new File("C:\\Users\\admin\\Desktop\\QuanLiSinhVien\\FileStudent.csv");
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             String value;
             while ((value = bufferedReader.readLine()) != null) {
                 String[] strings = value.split(",");
-                Student student = new Student(Integer.parseInt(strings[0]),strings[1],Integer.parseInt(strings[3]),strings[4],strings[5],Double.parseDouble(strings[5]));
+                Student student = new Student(Integer.parseInt(strings[0]),strings[1],Integer.parseInt(strings[2]),strings[3],strings[4],Double.parseDouble(strings[5]));
                 students.add(student);
                 System.out.println("Đọc thành công");
             }
